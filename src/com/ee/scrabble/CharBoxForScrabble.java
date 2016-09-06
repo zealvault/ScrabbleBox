765f420b7478f81482a0cd66a61646a1e1070017
zealvault <vishalkumarsingh12640@gmail.com>
2016-09-06 14:37:58 +0530

    Add files via upload

--- src/com/ee/scrabble/CharBoxForScrabble.java
+++ src/com/ee/scrabble/CharBoxForScrabble.java
@@ -10,47 +10,47 @@ import java.util.Random;
 
 public class CharBoxForScrabble {
     private final int MAX_CHARS_IN_BOX = 50;
-    private List<Character> boxOfchars;
+    private List<Character> boxOfChars;
 
     public CharBoxForScrabble() {
+
         fillCharBox();
     }
 
     private void fillCharBox() {
-        this.boxOfchars = new LinkedList<>();
+        this.boxOfChars = new LinkedList<>();
         addRandomCharsToBoxOfChars();
     }
 
     private void addRandomCharsToBoxOfChars() {
         Random randomObject = new Random();
         for (int boxIterator = 0; boxIterator < MAX_CHARS_IN_BOX; boxIterator++) {
-            this.boxOfchars.add((char)('a' + randomObject.nextInt(26)));
+            this.boxOfChars.add((char)('a' + randomObject.nextInt(26)));
         }
     }
 
-    public char[] getStreamOfCharsToPlay(int requestedNumberOfChars) {
-
-        if(this.boxOfchars.size()<requestedNumberOfChars){
-            throw new IllegalArgumentException("Request fewer than "+this.boxOfchars.size());
+    public List<Character> getStreamOfCharsToPlay(int requestedNumberOfChars) {
+        List<Character> resultantListOfChars = new LinkedList<>();
+        if(this.boxOfChars.size()<requestedNumberOfChars){
+            throw new IllegalArgumentException("Request fewer than "+this.boxOfChars.size());
         }
         Random randomObject = new Random();
-        char[] resultantCharArray = new char[requestedNumberOfChars];
         int numberOfCharsInResultantCharArray = 0;
         while (numberOfCharsInResultantCharArray < requestedNumberOfChars) {
-            int randomNumber = randomObject.nextInt(this.boxOfchars.size()-1);
-            resultantCharArray[numberOfCharsInResultantCharArray] = this.boxOfchars.get(randomNumber);
+            int randomNumber = randomObject.nextInt(this.boxOfChars.size());
+            resultantListOfChars.add(this.boxOfChars.get(randomNumber));
             removeCharFromBox(randomNumber);
             numberOfCharsInResultantCharArray++;
         }
-        return resultantCharArray;
+        return resultantListOfChars;
     }
 
-    private void removeCharFromBox(int indexOfcharacter) {
-        this.boxOfchars.remove(indexOfcharacter);
+    private void removeCharFromBox(int indexOfCharacter) {
+        this.boxOfChars.remove(indexOfCharacter);
     }
 
     @Override
     public String toString() {
-        return (this.boxOfchars).toString();
+        return (this.boxOfChars).toString();
     }
 }
--- src/com/ee/scrabble/Scrabble.java
+++ src/com/ee/scrabble/Scrabble.java
@@ -1,28 +1,37 @@
 package com.ee.scrabble;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Vishal on 05-09-2016.
  */
 public class Scrabble {
     private static Scrabble onlyInstanceOfScrabble;
     private CharBoxForScrabble charBox = null;
-
+    List<Character> characterList;
 
     private  Scrabble(){
 
-        charBox = getAlphabetsForGame();
+        charBox = new CharBoxForScrabble();
+        //charBox = getAlphabetsForGame();
     }
 
-    private CharBoxForScrabble getAlphabetsForGame() {
+//    private CharBoxForScrabble getAlphabetsForGame() {
+//
+//        return new CharBoxForScrabble();
+//    }
 
-        return new CharBoxForScrabble();
-    }
+    public static synchronized Scrabble getInstance(int numberOfChars) {
+        synchronized (Scrabble.class){
+            if(onlyInstanceOfScrabble ==null){
+                onlyInstanceOfScrabble = new Scrabble();
+            }
+            onlyInstanceOfScrabble.characterList = new LinkedList<>();
+            onlyInstanceOfScrabble.characterList = onlyInstanceOfScrabble.charBox.getStreamOfCharsToPlay(numberOfChars);
 
-    public synchronized static Scrabble getInstance() {
-        if(onlyInstanceOfScrabble ==null){
-            onlyInstanceOfScrabble = new Scrabble();
         }
+
         return onlyInstanceOfScrabble;
     }
-
 }
